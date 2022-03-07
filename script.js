9dcd52e0cde92ffc7a689b0612a7a7700a8a28ce
abubakergul <abubakergul24@gmail.com>
2022-03-07 19:01:26 +0500

    Intial state

---
+++ script.js
@@ -0,0 +1,12 @@
+const massMark = 78;
+const massJohn = 92;
+const heightMark = 1.69;
+const heightJohn = 1.95;
+const markBMI = massMark / heightMark ** 2;
+const johnBMI= massJohn / heightJohn **2;
+
+const markHigherBMI=(markBMI > johnBMI);
+console.log(markBMI);
+console.log(johnBMI);
+console.log(markHigherBMI);
+
